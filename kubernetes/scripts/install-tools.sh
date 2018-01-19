2d3ddec36acfacb41a03eeddd46a4d7ddfa5d891
Dkhil <Chiheb.Dkhil@emc.com>
2018-01-19 08:48:11 -0500

    t

--- kubernetes/scripts/install-tools.sh
+++ kubernetes/scripts/install-tools.sh
@@ -7,6 +7,6 @@ export KOPS_STATE_STORE=s3://prefix-chiheb-dkhil-state-store
 cp -rf output/.kube /root/
 cp -rf output/.helm /root/
 
-sleep 240
+sleep 20
 kops delete cluster --name ${NAME} --yes
 sleep 100
