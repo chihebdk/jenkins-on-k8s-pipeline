17b99dc432ef908017c218878c90278df5233fd1
Dkhil <Chiheb.Dkhil@emc.com>
2018-01-18 22:03:26 -0500

    u

--- kubernetes/scripts/create-cluster.sh
+++ kubernetes/scripts/create-cluster.sh
@@ -30,6 +30,10 @@ if [ $(aws s3api list-buckets | jq .Buckets[].Name | grep -w \"prefix-chiheb-dkh
 fi
 aws s3api create-bucket  --bucket prefix-chiheb-dkhil-state-store --region us-east-1
 
+
+export NAME=myfirstcluster.k8s.local
+export KOPS_STATE_STORE=s3://prefix-chiheb-dkhil-state-store
+
 #Recreate key-pair
 aws ec2 delete-key-pair --key-name kops
 aws ec2 create-key-pair --key-name kops --query 'KeyMaterial' --output text > kops.pem
